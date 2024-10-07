09cc37a00c019fd50a9b245ff12d997887f55932
TThai <163569473+Triutheis@users.noreply.github.com>
2024-10-07 21:21:25 +0700

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -4,9 +4,9 @@ apt update && apt upgrade -y
 clear
 cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && rm v4.zip && pkg install wget -y && wget https://github.com/Triutheis/v4/raw/main/v4.zip && unzip v4.zip && chmod a+x tun && chmod a+x psiphon-tunnel-core && chmod a+x qexit && chmod a+x wl && chmod a+x ver && chmod a+x ver2 && chmod a+x v4man && chmod a+x qoa && chmod a+x v4a
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ && rm t1 && pkg install wget -y && wget https://github.com/Triutheis/v4/raw/main/t1 && mv t1 motd
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ && pkg install wget -y && wget https://github.com/Triutheis/v4/raw/main/t1 && mv t1 motd
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && rm l1 && pkg install wget -y && wget https://github.com/Triutheis/v4/raw/main/l1 && chmod +x * && mv l1 login &&
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/Triutheis/v4/raw/main/l1 && chmod +x * && mv l1 login &&
 clear
 echo "Đã cài đặt thành công"
 login
